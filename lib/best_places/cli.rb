9cdef3bb6a34a1920c8bd7292fc662e37616498e
Sequina Hall <sccaro89@gmail.com>
2017-06-01 19:39:49 -0500

    working on scraping attributes

--- lib/best_places/cli.rb
+++ lib/best_places/cli.rb
@@ -1,33 +1,21 @@
 class BestPlaces::CLI
 
-  # def list_places
-  #   puts "Welcome to the best places on Earth!"
-  #   BestPlaces::Places.scrape_places
-  # end
-  #
-  # def call
-  #   list_places
-  #   menu
-  #   goodbye
-  # end
-
-
 def menu
     input = nil
     puts "Enter something - testing: "
     puts "Type exit to exit the program"
 
-    # while input != "exit"
-    #   input = gets.strip.downcase
-    #   case input
-    #     when "xbox"
-    #       puts "List of XBOX games with release dates"
-    #     when "ps4"
-    #       puts "List of XBOX games with release dates"
-    #     else
-    #       puts "Please choose a console from the list above"
-    #     end
-    #   end
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+          puts "#{weather}"
+        when "ps4"
+          puts "List of XBOX games with release dates"
+        else
+          puts "Please choose a console from the list above"
+        end
+      end
     end
 
   def goodbye
--- lib/best_places/places.rb
+++ lib/best_places/places.rb
@@ -4,22 +4,32 @@ require 'open-uri'
 module BestPlaces
 
   class Places < CLI
-    attr_accessor :name, :population, :places
+    attr_accessor :name, :population, :places, :weather
 
       def initialize
         @places = []
+        @weather = []
       end
 
       def scrape_places
         doc = Nokogiri::HTML(open("https://nomadlist.com/best-cities-to-live"))
         places = doc.search("div.text h2.itemName")
         ranks = doc.search("div.rank")
+        # weather = doc.search("div.attributes div.bottom-left.weather")
+        weather = doc.search("div.attributes div.bottom-left.weather span.value")
+        # binding.pry
         places.each{|e| @places << e.text.strip}
 
           @places.each do |place|
             i = @places.index(place)
           puts "#{ranks[i].text}. #{place}"
         end
+
+        weather.each{|w| @weather << w.text.strip}
+          @weather.each do |w|
+            i = @weather.index(weather)
+            puts "#{weather}"
+          end
       end
     end
 
