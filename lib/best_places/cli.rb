8188868a1da7d8a4d92405f2ac7afe65bd8aef24
Sequina Hall <sccaro89@gmail.com>
2017-06-02 20:23:03 -0500

    working on contstant

--- lib/best_places/cli.rb
+++ lib/best_places/cli.rb
@@ -1,24 +1,27 @@
-class BestPlaces::CLI
+module BestPlaces
+ class CLI < Places
 
-def menu
-    input = nil
-    puts "Enter something - testing: "
-    puts "Type exit to exit the program"
+    def menu
+      input = nil
+      puts "Enter something - testing: "
+      puts "Type exit to exit the program"
 
-    while input != "exit"
-      input = gets.strip.downcase
-      case input
-      when "1"
-          puts "#{weather}"
-        when "ps4"
-          puts "List of XBOX games with release dates"
-        else
-          puts "Please choose a console from the list above"
+      while input != "exit"
+        input = gets.strip.downcase
+        case input
+        when "1"
+            puts "#{ranks[0]}. #{@@air_quality}"
+          when "ps4"
+            puts "List of XBOX games with release dates"
+          else
+            puts "Please choose a console from the list above"
+          end
         end
       end
+
+    def goodbye
+      puts "See you later!"
     end
 
-  def goodbye
-    puts "See you later!"
   end
 end
--- lib/best_places/places.rb
+++ lib/best_places/places.rb
@@ -4,19 +4,34 @@ require 'open-uri'
 module BestPlaces
 
   class Places < CLI
-    attr_accessor :name, :population, :places, :weather
+    attr_accessor :name, :population, :places, :air_quality
+    @@air_quality = []
 
       def initialize
         @places = []
-        @weather = []
       end
+
+      # def self.all
+      #   @@all ||= scrape_places
+      # end
+      #
+      # def self.find(weather)
+      #   self.all.detect do |w|
+      #     weather.each{|e| @weather << e.text.strip}
+      #       @weather.each do |w|
+      #         i = @weather.index(w)
+      #         puts "#{weather[i].text}"
+      #       end
+      #     end
+      #   end
+      # end
 
       def scrape_places
         doc = Nokogiri::HTML(open("https://nomadlist.com/best-cities-to-live"))
         places = doc.search("div.text h2.itemName")
         ranks = doc.search("div.rank")
-        # weather = doc.search("div.attributes div.bottom-left.weather")
-        weather = doc.search("div.attributes div.bottom-left.weather span.value")
+        air_quality = doc.search("div.attributes div.bottom-left.weather span.air_quality")
+
         # binding.pry
         places.each{|e| @places << e.text.strip}
 
@@ -25,13 +40,18 @@ module BestPlaces
           puts "#{ranks[i].text}. #{place}"
         end
 
-        weather.each{|w| @weather << w.text.strip}
-          @weather.each do |w|
-            i = @weather.index(weather)
-            puts "#{weather}"
+        air_quality.each{|a| @@air_quality << a.text.strip}
+
+          @@air_quality.each do |air|
+            i = @@air_quality.index(air)
+            # puts "#{ranks[i].text}. #{@@air_quality}"
           end
+        end
       end
-    end
+
+      # def scrape_attributes
+      #   doc = Nokogiri::HTML(open("https://nomadlist.com/best-cities-to-live"))
+
 
      class CLI
 
@@ -45,6 +65,5 @@ module BestPlaces
          menu
          goodbye
        end
-
      end
   end
